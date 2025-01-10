650b41c28a00fa0c3774a6742c006839d186c9a5
Jhoset NG <adm_dng@hotmail.com>
2025-01-09 22:48:29 -0500

    feat: combine streams - chapter5

--- Chap03/recipes-book-front/src/app/core/services/recipes.service.ts
+++ Chap03/recipes-book-front/src/app/core/services/recipes.service.ts
@@ -2,7 +2,7 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Recipe} from '../model/recipe.model';
 import {environment} from 'src/environments/environment';
-import {catchError, of} from "rxjs";
+import {BehaviorSubject, catchError, of} from "rxjs";
 
 const BASE_PATH = environment.basePath
 
@@ -13,10 +13,22 @@ const BASE_PATH = environment.basePath
 
 export class RecipesService {
 
+  //? Create the action stream - filterRecipeSubject
+  private filterRecipeSubject = new BehaviorSubject<Recipe>({title: ''});
+  //* Extract the readonly stream
+  //* Consumers can only read the stream but not write to it using the next() method
+  filterRecipesAction$ = this.filterRecipeSubject.asObservable();
+
+  //! Data Stream - recipes$
   recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`).pipe(
     catchError(() => of([]))
   );
 
+  updateFilter(criteria: Recipe) {
+    this.filterRecipeSubject.next(criteria);
+  }
+
   constructor(private http: HttpClient) {
+
   }
 }
--- Chap03/recipes-book-front/src/app/recipes-list/recipes-list.component.ts
+++ Chap03/recipes-book-front/src/app/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RecipesService} from '../core/services/recipes.service';
 import {DataViewModule} from 'primeng/dataview';
@@ -9,8 +9,22 @@ import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {RippleModule} from 'primeng/ripple';
 import {RatingModule} from 'primeng/rating';
-import {FormsModule} from '@angular/forms';
-import {catchError, delay, delayWhen, from, map, of, retry, retryWhen, tap, throwError, timer} from "rxjs";
+import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  catchError, combineLatest,
+  delay,
+  delayWhen,
+  from, interval,
+  map,
+  of,
+  retry,
+  retryWhen,
+  Subject,
+  takeUntil,
+  tap,
+  throwError,
+  timer, withLatestFrom
+} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../core/model/recipe.model";
 
@@ -25,15 +39,23 @@ import {Recipe} from "../core/model/recipe.model";
     InputTextModule,
     ButtonModule,
     RippleModule,
-    RatingModule, FormsModule
+    RatingModule, FormsModule, ReactiveFormsModule
   ],
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RecipesListComponent {
+
+
   recipes$ = this.service.recipes$;
+
+  filterRecipesAction$ = this.service.filterRecipesAction$;
+  filteredRecipes$ = combineLatest([this.recipes$, this.filterRecipesAction$])
+    .pipe(map(([recipes, filter]: [Recipe[], Recipe]) => {
+      const filterTitle = filter?.title?.toLowerCase() ?? '';
+      return recipes.filter((recipe) => recipe.title?.toLowerCase().includes(filterTitle))
+    }));
 
   stream$ = from(['5', '10', '6', 'Hello', '123']);
   streamOfNumbers$ = this.stream$.pipe(map((value: string) => {
@@ -43,14 +65,75 @@ export class RecipesListComponent {
   }));
 
   constructor(private service: RecipesService,
-              private http: HttpClient) {
+              private http: HttpClient,
+              private fb: FormBuilder) {
+
 
     //this.replaceStrategy();
     //this.rethrowStrategy();
     //this.retryStrategy();
     //this.retryWhenStrategy();
     //this.retryWhenAndDelayWhenForRetryStrategy();
+
+    //this.sensorsSimulation();
+    //this.priceComparator();
+    //this.gameSimulation();
   }
+
+  // ngOnInit() {
+  //   this.service.recipes$.pipe(takeUntil(this.destroy$)).subscribe((recipes) => {
+  //     this.recipes = recipes;
+  //     this.filteredRecipes = recipes;
+  //   })
+  // }
+
+
+  // filterResults(r: Recipe) {
+  //   if (!r.title) return;
+  //   this.filteredRecipes = this.recipes.filter(recipe => recipe.title?.indexOf(r.title!) !== 1)
+  // }
+
+  public gameSimulation() {
+    const gameTimer$ = interval(1000);
+    const playerEnergy$ = interval(3000).pipe(map((value) => {
+      console.log('>>> energy checking:', value);
+      if (value === 2) throw new Error('Player energy is low');
+      return Math.floor(Math.random() * 100);
+    }));
+
+    gameTimer$.pipe(
+      withLatestFrom(playerEnergy$),
+      map(([time, playerEnergy]) => {
+        return `Time: ${time}, Player Energy: ${playerEnergy}`;
+      })
+    ).subscribe(console.log)
+  }
+
+  public priceComparator() {
+    const store1$ = interval(1500).pipe(map(() => Math.floor(Math.random() * 100) + 1));
+    const store2$ = interval(1500).pipe(map(() => Math.floor(Math.random() * 100) + 1));
+
+    combineLatest([store1$, store2$]).pipe(
+      map(([store1, store2]) => store1 > store2 ? `Store 1 is cheaper ${store1}` : `Store 2 is cheaper ${store2}`)
+    ).subscribe(data => {
+      console.log('Result:', data);
+    })
+  }
+
+  public sensorsSimulation() {
+
+    const temperature$ = interval(2000).pipe(map(() => Math.floor(Math.random() * 35) + 15));
+    const humidity$ = interval(3000).pipe(map(() => Math.floor(Math.random() * 100)));
+
+    combineLatest([temperature$, humidity$]).pipe(
+      map(([temperature, humidity]) => {
+        return {temperature, humidity};
+      })
+    ).subscribe(data => {
+      console.log('Temperature and humidity', data);
+    })
+  }
+
 
   public replaceStrategy() {
     console.log('\x1b[36m%s\x1b[0m', '>>> The replace Strategy');
